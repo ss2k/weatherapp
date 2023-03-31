5eedf8a3c0e76dbf03244ef635c5b9c65b836aed
SS <shahriarsikder@gmail.com>
2023-04-01 01:18:53 +0300

    Update WeatherData.jsx

--- app/javascript/components/WeatherData.jsx
+++ app/javascript/components/WeatherData.jsx
@@ -2,13 +2,11 @@ import React from "react";
 
 const WeatherData = ({ data }) => {
   return(
-    <>
       <div className="weather_box">
         <h1>{data.weather}{data.cache && <> - &Delta;</>}</h1>
         <span className="temp">{data.temp}&deg;F</span>
         <span className="high-low">{data.min}&deg;F / {data.max}F&deg;</span>
       </div>
-    </>
   )
 }
 
